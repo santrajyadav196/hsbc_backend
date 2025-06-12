fd49487e121e054a19f82b6cd99a8b5e12039e46
santrajyadav196 <santrajyadav196@gmail.com>
2025-06-12 11:39:21 +0530

    mod in index file

--- index.js
+++ index.js
@@ -17,12 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public folder
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Change this to your frontend URL
+  // origin: "http://localhost:5173", // Change this to your frontend URL
+  origin: "*", // Allow all origins
   credentials: true,
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
 };
 app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.use("/api/auth", userRoutes);
 app.use("/api/expenses", expenseRoutes);
